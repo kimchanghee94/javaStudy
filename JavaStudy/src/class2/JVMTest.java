8b91a8d47df3685c61eac72a4a3a4726e55b3459
kimchanghee94 <kimchanghee94@naver.com>
2022-09-20 19:38:52 +0900

    JVMTest를 위한 간략한 코드 작성(추가 작업 진행 필요)

---
+++ JavaStudy/src/class2/JVMTest.java
@@ -0,0 +1,7 @@
+package class2;
+
+public class JVMTest {
+	public static void main(String[] args) {
+		System.out.println("This is JVMTEST Main Code");
+	}
+}
---
+++ JavaStudy/src/class2/JVMTest2.java
@@ -0,0 +1,7 @@
+package class2;
+
+public class JVMTest2 {
+	public static void main(String[] args) {
+		System.out.println("This is JVMTEST2 Main Code");
+	}
+}
