4249b596c26010850f66ed8f32fe5ace373e7405
kimchanghee94 <kimchanghee94@naver.com>
2022-09-27 00:01:38 +0900

    jvm옵션 설정 수정하여 이클립스에서도 커스텀클래스로더 실행 완료

--- JavaStudy/src/class2/ClassLoaderMain.java
+++ JavaStudy/src/class2/ClassLoaderMain.java
@@ -2,6 +2,7 @@ package class2;
 
 public class ClassLoaderMain {
 	public static void main(String[] args) {
+		/*Run Configuration에 jvm 옵션설정 잘못한 부분 수정 완료*/
 		System.out.println("main is start");
 	}
 }
