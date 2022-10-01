57851615cceb03f227cee7a34c24d600ec4f2416
kimchanghee94 <kimchanghee94@naver.com>
2022-10-01 11:13:51 +0900

    클래스로더를 호출하는 것은 JVM이고 JVM에서 클래스로더를 호출하는 주체는 객체아닌가.. VM Class일까...

--- JavaStudy/src/class2/ClassLoaderMain.java
+++ JavaStudy/src/class2/ClassLoaderMain.java
@@ -3,6 +3,7 @@ package class2;
 public class ClassLoaderMain {
 	public static void main(String[] args) {
 		/*Run Configuration에 jvm 옵션설정 잘못한 부분 수정 완료*/
+		/* class loader를 누가 호출하는지 알 수없을까... 일단 넘기고 jvm정의 파악 진행해야되나... */
 		System.out.println("main is start");
 		
 		AClass ac = new AClass();
