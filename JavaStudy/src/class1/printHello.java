69b5ca82b24b95399098df73b8c283537617c87f
kimchanghee94 <kimchanghee94@naver.com>
2022-10-13 12:23:04 +0900

    클래스로드 블로그 작성 중에 궁금점 테스트

--- JavaStudy/src/class1/printHello.java
+++ JavaStudy/src/class1/printHello.java
@@ -3,5 +3,16 @@ package class1;
 public class printHello {
 	public static void main(String[] args) {
 		System.out.println("print Hello World");
+		
+		AbstTest at = new AbstTest() {
+			
+		};
+		at.testMethod();
+	}
+}
+
+abstract class AbstTest{
+	public void testMethod() {
+		System.out.println("abstTest");
 	}
 }
